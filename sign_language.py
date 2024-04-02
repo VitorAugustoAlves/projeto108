d89820bb26162487e6dda1d2669168225cd8abac
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2024-04-02 16:01:28 -0300

    Add files via upload

--- sign_language.py
+++ sign_language.py
@@ -23,26 +23,30 @@ while True:
             for id ,lm in enumerate(hand_landmark.landmark):
                 lm_list.append(lm)
 
-             #O código vai aqui  
+             #O código vai aqui
+            finger_fold_status = []    
+
             for tip in finger_tips:
                 x,y = int(lm+list[tip].x*w), int(lm+list[tip].y*h)
                 cv2.circle(img,(x,y),15, (0,255,0), cv2.FILLED)
 
-            if lm_list[tip].x < lm_list[tip-3].x:    
-                cv2.circle(img,(x,y),15, (0,255,0), cv2.FILLED)
-                finger_fold_status.append(True)
+                if lm_list[tip].x < lm_list[tip-3].x:    
+                    cv2.circle(img,(x,y),15, (0,255,0), cv2.FILLED)
+                    finger_fold_status.append(True)
 
-            else:
-                finger_fold_status.append(False)
+                else:
+                    finger_fold_status.append(False)
+
+                print(finger_fold_status)
 
             if all(finger_fold_status):
                 if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_tip-2].y:
                     print("CURTI")
-                    cv2.putText(img ,"CURTI", (20,30), cv2.FONT HERSHEY SIMPLEX, 1, (0,255,0), 3)   
+                    cv2.putText(img ,"CURTI", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)   
 
                 if lm_list[thumb_tip].y > lm_list[thumb_tip-1].y > lm_list[thumb_tip-2].y:
                     print("NÃO CURTI")
-                    cv2.putText(img ,"NÃO CURTI", (20,30), cv2.FONT HERSHEY SIMPLEX, 1, (0,0,255), 3) 
+                    cv2.putText(img ,"NÃO CURTI", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3) 
 
         mp_draw.draw_landmarks(img, hand_landmark,
         mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
